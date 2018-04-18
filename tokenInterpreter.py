5249e53221a207c8bc26fe01e3c7f8f310d121fb
BALJ55 <byronamh@gmail.com>
2018-04-17 20:26:51 -0600

    metodo para obtener nombre del token, inicio de implementación de alter table

--- tokenInterpreter.py
+++ tokenInterpreter.py
@@ -14,13 +14,27 @@ class tokenInterpreter(sqlListener):
     def exitR(self, ctx):
         print("Hello world. At the input has been already validated")
 
+    def getTokenValue(self, name):
+        return name.any_name().IDENTIFIER()
+
 
 # CREATE SECTION
     def enterCreate_database_stmt(self, ctx: sqlParser.Create_database_stmtContext):
-        print("Nombre de la tabla")
-        databaseName = ctx.database_name().any_name().IDENTIFIER()
+        # print("Nombre de la tabla")
+        databaseName = self.getTokenValue(ctx.database_name())
+        print(databaseName)
     #     @TODO: IMPLEMENTAR CREADO DE CARPETA
+    #     @TODO: IMPLEMENTAR CREADO DE DBS EN CARPETA
 
     def exitCreate_database_stmt(self, ctx: sqlParser.Create_database_stmtContext):
+        print("DATABASE CREATE EXECUTED")
         pass
 # !CREATE SECTION
+# ALTER TABLE SECTION
+    def enterAlter_database_stmt(self, ctx: sqlParser.Alter_database_stmtContext):
+        target_database = ctx.database_name().any_name().IDENTIFIER()
+        pass
+
+    def exitAlter_database_stmt(self, ctx: sqlParser.Alter_database_stmtContext):
+        pass
+# !ALTER TABLE SECTION
